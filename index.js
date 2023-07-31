91095d55213a751562ea54a02fc71bbb54c83cd4
Muhammad Rafly Sadewa <msadewa@pajak.io>
2023-07-31 16:38:11 +0700

    update finishing timestamp project

--- index.js
+++ index.js
@@ -2,31 +2,65 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+// your first API endpoint...
+// app.get("/api/hello", function (req, res) {
+//   res.json({ greeting: "hello API" });
+// });
+app.get("/api/", function (req, res) {
+  let responseDate = new Date();
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({
+    unix: responseDate.getTime(),
+    utc: responseDate.toUTCString(),
+  });
 });
+app.get("/api/:date", function (req, res) {
+  const isMilliSeconds = (value) => {
+    return (
+      typeof Number(value) === "number" && !isNaN(value) && Number(value) > 0
+    );
+  };
 
+  const isDateString = (value) => {
+    return typeof value === "string" && !isNaN(Date.parse(value));
+  };
 
+  if (isMilliSeconds(req.params.date)) {
+    const responseDate = new Date(Number(req.params.date));
+    res.json({
+      unix: responseDate.getTime(),
+      utc: responseDate.toUTCString(),
+    });
+    return;
+  }
+  if (isDateString(req.params.date)) {
+    const responseDate = new Date(req.params.date);
+    res.json({
+      unix: responseDate.getTime(),
+      utc: responseDate.toUTCString(),
+    });
+    return;
+  }
+  res.json({ error: "Invalid Date" });
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
